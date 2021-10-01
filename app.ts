70befdabe3ec521ff42475ac7f5bb3247eb317b9
lucascplusmart <lucascplusmartcplu@gmail.com>
2021-10-01 09:11:49 -0300

    [node-chat-01] - adição da funcionalidade da data

--- app.ts
+++ app.ts
@@ -1,6 +1,11 @@
 import { io, http } from './server'
 import './socket/usuario'
+try {
+	http.listen(process.env.PORT, () => {
+		console.log(`Servidor rodando: http://localhost:${process.env.PORT}`)
+	})
+}
+catch (err) {
+	console.log(`Verifique a conexão: ${err}`)
+}
 
-http.listen(process.env.PORT, () => {
-	console.log(`Servidor rodando na porta ${process.env.PORT}`)
-})
